62434437be0cabe37bc450e187e05cb016929126
emjshrx <emjshrx@gmail.com>
2023-10-08 09:39:55 +0530

    feat: scaffold app

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      black: "#333333",
+      white: "#F7F7F7",
+      orange: "#FA8816",
+      peach: "#FFDEC7",
+    },
+    fontFamily: {
+      header: "Firs Neue, sans-serif",
+      base: "AeonikFono, sans-serif",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
