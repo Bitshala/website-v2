c7d7283246175b43618385a8b812eb103231a060
emjshrx <emjshrx@gmail.com>
2023-10-08 09:46:24 +0530

    feat: deploy to pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+   site: 'https://bitshala.github.io',
+  base: '/my-repo',
   integrations: [tailwind()],
 });
