59d8e51abda6148a664d0014d46ae55a457dc7c2
emjshrx <emjshrx@gmail.com>
2023-11-24 13:43:58 +0530

    refac: change faq to a generic component

---
+++ src/components/CollapseList.tsx
@@ -0,0 +1,58 @@
+import Collapse from "@mui/material/Collapse";
+import { useState } from "react";
+
+export const CollapseList = ({
+  header,
+  listConfig,
+}: {
+  header: string;
+  listConfig: { q: any; a: any }[];
+}) => {
+  const [activeIndex, setActiveIndex] = useState(-1);
+  const handleElementClick = (index: number) => {
+    if (activeIndex == index) {
+      setActiveIndex(-1);
+    } else {
+      setActiveIndex(index);
+    }
+  };
+  return (
+    <>
+      <div className="mb-12 mt-6 text-5xl font-bold">
+        {header}
+      </div>
+      <hr className="mb-8 border-spacing-12 border-dashed" />
+      <ul>
+        {listConfig.map((el, index) => {
+          return (
+            <li key={el.q} className="text-2xl">
+              <button
+                className="flex h-full w-full items-center justify-between p-4"
+                onClick={() => handleElementClick(index)}
+              >
+                {el.q}
+                <div
+                  className={` hidden text-3xl transition-transform lg:block ${
+                    activeIndex == index ? `rotate-45` : ``
+                  }`}
+                >
+                  +
+                </div>
+              </button>
+              <Collapse
+                in={activeIndex == index}
+                timeout="auto"
+                unmountOnExit
+              >
+                <div className="flex justify-between p-4">
+                  {el.a}
+                </div>
+              </Collapse>
+              <hr className="my-8 border-spacing-12 border-dashed" />
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
