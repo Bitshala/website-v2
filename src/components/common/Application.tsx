0a8023ff669819960ae13380d0545c9ebfc0512c
keraliss <keralis1131f@gmail.com>
2024-02-15 19:29:09 +0530

    updated question and input ui change

--- src/components/common/Application.tsx
+++ src/components/common/Application.tsx
@@ -1,23 +1,40 @@
-import React, { useState, FormEvent } from "react";
+import React, { useState, type FormEvent } from "react";
 import axios from "axios";
 
 const Application: React.FC = () => {
-  const [userName, setUserName] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [role, setRole] = useState<string>("");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    location: "",
+    year: "",
+    background: "",
+    time: "",
+    why: "",
+    // readMB: "",
+    // work: "",
+    // role: "",
+    // expectation: "",
+    // bitcoinCrypto: "",
+    enrolled: false,
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
     console.log("Starting");
-    const userData = {
-      name: userName,
-      email: email,
-      role: role,
-    };
-    console.log("userdata", userData);
-    axios.post("http://13.232.129.218/register", userData).then((response) => {
-      console.log(response);
-    });
+    console.log("userdata", formData);
+    // axios.post("http://13.232.129.218/register", formData).then((response) => {
+    //   console.log(response);
+    // });
     console.log("done");
   };
 
@@ -25,35 +42,83 @@ const Application: React.FC = () => {
     <>
       <section className="grid place-items-center my-10 ">
         <h3 className="flex h-14 cursor-pointer items-center font-bold lg:text-4xl">
-          Found the cohort interesting? Click here to apply!
+         <span className="text-orange px-2">Register</span> for the cohort now!
         </h3>
 
-        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="username"
-              onChange={(e) => setUserName(e.target.value)}
-            />
-          </label>
-          <label>
-            Email:
-            <input
-              type="email"
-              name="email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </label>
-          <label>
-            Role:
-            <input
-              type="text"
-              name="role"
-              onChange={(e) => setRole(e.target.value)}
-            />
-          </label>
-          <input type="submit" value="Submit" />
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col px-5 pt-2 gap-1 rounded-lg lg:w-1/2"
+        >
+          <p>Name/Pseudonym*</p>
+          <input
+            className="border text-sm rounded-lg block w-full p-2.5 mb-3"
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+          <p>Email*</p>
+          <input
+            className="border text-sm rounded-lg block w-full p-2.5 mb-3"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <p>Location*</p>
+          <input
+            className="border text-sm rounded-lg block w-full p-2.5 mb-3"
+            type="text"
+            name="location"
+            value={formData.location}
+            onChange={handleChange}
+            required
+          />
+          <p>Which year did you start taking Bitcoin seriously?*</p>
+          <input
+            className="border text-sm rounded-lg block w-full p-2.5 mb-3"
+            type="text"
+            name="year"
+            value={formData.year}
+            onChange={handleChange}
+            required
+          />
+          <p>Skills</p>
+          <select className="border text-sm rounded-lg block w-full p-2.5 mb-3 bg-white">
+            <option className="bg-white" value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          <p>Portfolio/Github/Side-project link:</p>
+          <input
+            className="border text-sm rounded-lg block w-full p-2.5 mb-3"
+            type="text"
+            name="background"
+            value={formData.background}
+            onChange={handleChange}
+            required
+          />
+          <p>How many hours per week are you willing to dedicate to this cohort?</p>
+          <input
+            className="border text-sm rounded-lg block w-full p-2.5 mb-3"
+            type="text"
+            name="time"
+            value={formData.time}
+            onChange={handleChange}
+            required
+          />
+          <p>What do you hope to achieve through this Cohort?</p>
+          <textarea
+            className="border text-sm rounded-lg block w-full p-2.5 mb-3"
+            name="why"
+            value={formData.why}
+            onChange={handleChange}
+          />
+          <button type="submit" className="bg-orange text-white p-2 rounded-lg my-5 hover:text-black hover:bg-peach lg:w-1/3 lg:self-center">Apply</button>
         </form>
       </section>
     </>
