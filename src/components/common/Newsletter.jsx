0703cd56c842f95fe75e0c9d54eeb34d3b5475de
keraliss <keralis1131f@gmail.com>
2024-04-05 11:47:40 +0530

    added client load, and handled the after-submit notification.

--- src/components/common/Newsletter.jsx
+++ src/components/common/Newsletter.jsx
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 
 const Newsletter = () => {
   const [email, setEmail] = useState("");
+  const [submitted, setSubmitted] = useState(true);
   const data = {
     Email: email,
   };
-  const handleClick = () => {
-    fetch(
+  const handleClick = async () => {
+    await fetch(
       "https://sheet.best/api/sheets/689179b9-ecd8-43ff-bb0d-0450a951a8d3",
       {
         method: "POST",
@@ -17,26 +18,35 @@ const Newsletter = () => {
         body: JSON.stringify(data),
       }
     );
+    setSubmitted(true);
+    setEmail("");
   };
   return (
     <div className="my-10 rounded-xl bg-[#fff] p-10 shadow-lg flex flex-col items-center justify-center">
       <p className="lg:text-5xl text-2xl my-5 font-header">
         Subscribe to our Newsletter to stay up to date
       </p>
-
-      <input
-        type="email"
-        placeholder="satoshin@gmx.com"
-        className="my-2 w-full lg:px-5 py-3 lg:w-1/2 rounded-lg border-2 border-black"
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <button
-        onClick={handleClick}
-        className="text-m mt-2 border-dashed border-2 rounded-lg bg-white p-4 text-center font-semibold hover:bg-orange hover:text-white lg:w-1/3 lg:text-2xl"
-      >
-        Subscribe
-      </button>
+      {submitted ? (
+        <p className="text-xl lg:text-2xl">
+          You have successfully subscribed to the Newsletter.
+        </p>
+      ) : (
+        <>
+          <input
+            type="email"
+            placeholder="satoshin@gmx.com"
+            className="my-2 w-full lg:px-5 py-3 lg:w-1/2 rounded-lg border-2 border-black"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <button
+            onClick={handleClick}
+            className="text-m mt-2 border-dashed border-2 rounded-lg bg-white p-4 text-center font-semibold hover:bg-orange hover:text-white lg:w-1/3 lg:text-2xl"
+          >
+            Subscribe
+          </button>
+        </>
+      )}
     </div>
   );
 };
