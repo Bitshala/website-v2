1cc55ef5da79c58a3ce4cb10b42eabf81c0795b0
keraliss <keralis1131f@gmail.com>
2024-06-10 20:33:47 +0530

    changed content and links added

---
+++ src/components/apprenticeship/Apply.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Apply = () => {
+  return (
+    <div>
+      {/* <div className="text-center font-header text-3xl text-black lg:text-7xl">
+        <span className="text-orange">Apply</span> to become
+        a Bitshala Fellow!
+      </div> */}
+      <div className="my-10 text-center">
+        <h1 className="font-header text-3xl font-bold text-orange lg:text-5xl">
+          Application is not open right now
+        </h1>
+        <p className="my-2 lg:text-xl">
+          Please keep an eye on the website for more updates
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Apply;
